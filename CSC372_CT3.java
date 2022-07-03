61f662047c761f5352cddcfc2398595553bd2783
Joshua James <thecoloradothompsons@gmail.com>
2022-07-03 08:33:37 -0600

    More comments

--- CSC372_CT3.java
+++ CSC372_CT3.java
@@ -1,8 +1,22 @@
+/*
+Creating a User Interface II
+Create a user interface that has a top bar that includes a menu. The menu should have four items.
+
+When the user selects the first menu option, then the date and time should be printed in a text box.
+
+When the user selects the second menu option, then the text box contents should be written to a text file named "log.txt."
+
+When the user selects the third menu item then the frame background color changes to random color hue of the color orange. 
+The menu option should display the initial random hue each time selected for a single execution of the program. 
+See https://www.w3schools.com/colors/colors_picker.asp (Links to an external site.)
+
+When the user selects the fourth menu option then the program exits.
+*/
+
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Random;
-
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -168,7 +182,7 @@ public class CSC372_CT3 extends Application {
       return orangeColors[colorNum];
 
    }
-   
+
    //Just a single line to launch it all
    public static void main(String [] args) {
       launch(args); 
