b93b7df285d6fec036e659650de53946073dce75
Joshua James <thecoloradothompsons@gmail.com>
2022-07-05 20:45:33 -0600

    Cone Implemented

---
+++ Cone.java
@@ -0,0 +1,29 @@
+public class Cone extends Shape{
+
+    public double radius;
+    public double height;
+
+    Cone(double radius, double height){
+        this.radius = radius;
+        this.height = height;
+    }
+
+    @Override
+    public double surface_area() {
+        //Cone Surface Area Formula
+        return (Math.PI * radius * (radius + Math.sqrt((Math.pow(height, 2)+Math.pow(radius, 2)))));
+    }
+
+    @Override
+    public double volume() {
+        //Cone Volume Formula
+        return (Math.PI * Math.pow(radius, 2) * (height / 3));
+    }
+
+    @Override
+    public String toString(){
+        //Override toString for convenient printing
+        return String.format("%.2f area, %.2f volume", this.surface_area(), this.volume());
+    }
+    
+}
