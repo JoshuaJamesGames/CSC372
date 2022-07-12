429c7cbd644683fd1c4a2dce90fd6d44574a6aa8
Joshua James <thecoloradothompsons@gmail.com>
2022-07-12 17:50:18 -0600

    CSC372_CT5 framework

---
+++ CSC372_CT5.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+/*
+Implementing Recursion to Provide a Product
+
+Using recursion, create a program that will allow a user to enter five numbers. 
+The program will provide the product of all five numbers using recursive methods. 
+ */
+
+
+
+public class CSC372_CT5 {
+    public static void main(String[] args) {
+        
+    }
+
+    //Intro
+    public void introduction(){
+
+    }
+
+    //Collect Input
+    public ArrayList<String> getInput(int numInputs){
+        
+        ArrayList<String> inputs = new ArrayList<String>();
+
+        return inputs;
+
+    }
+
+    //Recursive product function
+    public void recursiveProductFinder(){
+
+    }
+    
+    //Outtro
+    public void conclusion(){
+        
+    }
+
+}
