ee65fbeaf0af2b2e5a319ff38cf4de4a773276bb
Joshua James <thecoloradothompsons@gmail.com>
2022-07-12 18:34:44 -0600

    recursiveProductFinder() implemented

--- CSC372_CT5.java
+++ CSC372_CT5.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
 Implementing Recursion to Provide a Product
@@ -20,22 +21,42 @@ public class CSC372_CT5 {
     }
 
     //Collect Input
-    public ArrayList<String> getInput(int numInputs){
+    public ArrayList<Double> getInput(int numInputs){
         
-        ArrayList<String> inputs = new ArrayList<String>();
+        ArrayList<Double> inputs = new ArrayList<Double>();
+        Scanner scnr = new Scanner(System.in);
+        double userInput;
 
+        for(int i = 0; i < numInputs; i++){
+            System.out.println("What is input #" + (i+1) + ": ");
+            userInput = scnr.nextDouble();
+            inputs.add(userInput);
+        }
+        scnr.close();
         return inputs;
 
     }
 
     //Recursive product function
-    public void recursiveProductFinder(){
+    public double recursiveProductFinder(ArrayList<Double> inputList){
+        double product;
+        if(inputList.size() == 0){
+            return 0;
+        }
+        if(inputList.size() == 1){
+            product = inputList.get(0);
+            return product;
+        }else{
+            product = inputList.get(0);
+            inputList.remove(0);
+            return product * recursiveProductFinder(inputList);
+        }
 
     }
     
     //Outtro
     public void conclusion(){
-        
+
     }
 
 }
