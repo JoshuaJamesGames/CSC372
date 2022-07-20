f729f81011c772344865841802c70981dd622b81
Joshua James <thecoloradothompsons@gmail.com>
2022-07-19 19:44:19 -0600

    CSC372_CT6 Begun

---
+++ CSC372_CT6.java
@@ -0,0 +1,19 @@
+/*
+
+Create a Java program that will store 10 student objects in an ArrayList, ArrayList<Student>. 
+A student object consists of the following fields:
+
+int rollno
+String name
+String address
+
+Implement a comparator class to sort student objects by rollno (roll number). 
+Implement your own merge sort method and place your code in a separate Java source file. 
+Do not use a sort method from the Java collections library.
+
+*/
+
+
+public class CSC372_CT6 {
+    
+}
