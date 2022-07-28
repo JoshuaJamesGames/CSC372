3d641593915adc651008d14ac2343a6339b54792
Joshua James <thecoloradothompsons@gmail.com>
2022-07-28 08:45:20 -0600

    CSC372 Portfolio Project Layout

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+/*
+LinkedList for CSC372 Portfolio Project
+using Generic Type
+
+Sortable using a comparator
+*/
+
+public class LinkedList {
+    //Constructor
+
+    //add
+
+    //sort (with comparator)
+}
---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+/*
+Vehicle Class
+
+String make
+String model
+double miles per gallon
+
+Override toString for easy printing
+Include Comparators for sorting
+*/
+
+public class Vehicle {
+    
+}
