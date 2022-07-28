d1e11e05a5cdabd29a3609d46f49e8da171fa684
Joshua James <thecoloradothompsons@gmail.com>
2022-07-28 09:34:24 -0600

    Prototyping LinkedList

--- Vehicle.java
+++ Vehicle.java
@@ -10,5 +10,22 @@ Include Comparators for sorting
 */
 
 public class Vehicle {
-    
+
+    private String make;
+    private String model;
+    private double milesPerGallon;
+
+    Vehicle(String make, String model, double milesPerGallon){
+
+        this.make = make;
+        this.model = model;
+        this.milesPerGallon = milesPerGallon;
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("A %s %s gets %02f miles per gallon.", make, model, milesPerGallon);
+    }
+   
 }
